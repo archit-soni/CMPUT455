7f07c794728bf7ab9d19005d161ba1b110117e9e
Archit <archit2@ualberta.ca>
2022-11-03 17:17:52 -0600

    Init

---
+++ assignment3/NoGo.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+# /usr/bin/python3
+# Set the path to your python3 above
+
+from gtp_connection import GtpConnection
+from board_util import GoBoardUtil
+from board import GoBoard
+
+class Go0:
+    def __init__(self):
+        """
+        NoGo player that selects moves randomly from the set of legal moves.
+
+        Parameters
+        ----------
+        name : str
+            name of the player (used by the GTP interface).
+        version : float
+            version number (used by the GTP interface).
+        """
+        self.name = "Go0"
+        self.version = 1.0
+
+    def get_move(self, board, color):
+        return GoBoardUtil.generate_random_move(board, color, 
+                                                use_eye_filter=False)
+
+def run():
+    """
+    start the gtp connection and wait for commands.
+    """
+    board = GoBoard(7)
+    con = GtpConnection(Go0(), board)
+    con.start_connection()
+
+if __name__ == "__main__":
+    run()
